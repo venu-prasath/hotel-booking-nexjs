8036b3fa3d856c7e49acb65379d308dd9e17a0bf
venu-prasath <venuprasth133@gmail.com>
2024-09-11 03:15:18 -0400

    google authentication implemented

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "The Luxury Resorts | Account",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
   return (
     <div>
-      <h1>Accounts Page</h1>
+      <h1>Welcome, {session.user.name}</h1>
     </div>
   );
 }
